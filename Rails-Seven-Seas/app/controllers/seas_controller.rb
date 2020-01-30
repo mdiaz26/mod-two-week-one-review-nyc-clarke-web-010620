5a055f5eef14ba34474c5e5b76586a79a85ba788
mdiaz26 <mike.diaz006@gmail.com>
2020-01-30 17:53:20 -0500

    created edit and update methods

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,9 +1,26 @@
 class SeasController < ApplicationController
   #define your controller actions here
 
-  def seas
+  def index
     @seas = Sea.all
-    render "seas/index"
+    render :index
+  end
+
+  def show
+    @sea = Sea.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+    render :edit
+  end
+
+  def update
+    sea = Sea.find(params[:id])
+    sea.update(sea_params)
+  
+    redirect_to "/seas/#{sea.id}"
   end
 
   private
--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'seas', to: 'seas#seas'
+  resources :seas
+  
+  # get '/seas', to: 'seas#index'
+  # get '/seas/:id', to: 'seas#show'
+  # get '/seas/:id/edit', to: 'seas#edit'
 end
